4cbb5d89907c7363a8bbfb7d747ab504935751a6
zp <1207179599@qq.com>
2017-05-03 19:13:57 +0800

    添加图片

---
+++ src/util/DBUtils.java
@@ -0,0 +1,46 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBUtils {
+	public static Connection getConn() {
+	/*	Properties p = new Properties();
+		try {
+			p.load(is);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}*/
+
+		String driverclass = "com.mysql.jdbc.Driver";
+		String url = "jdbc:mysql:///ec_shop";
+		String user = "root";
+		String password = "root";
+		Connection conn = null;
+
+		try {
+			Class.forName(/* "com.mysql.jdbc.Driver" */driverclass);
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	public static void closeAll(ResultSet rs,PreparedStatement ps,Connection conn) throws SQLException{
+		if(rs!=null)
+			rs.close();
+		if(ps!=null){
+			ps.close();
+		if(conn!=null)
+			conn.close();
+		}
+	}
+
+	
+
+}
